0134eb59fdb82eb03bebfb00dbc9d78825598e10
Pedro Henrique <pedrosobralxv@gmail.com>
2016-05-26 10:26:09 -0300

    using the data to properly format card

--- app/components/route-card/route-card.ts
+++ app/components/route-card/route-card.ts
@@ -1,17 +1,26 @@
 import {Component, Input} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {RouteDetailsPage} from '../../pages/route-details/route-details';
+import {StopNamePipe} from '../../pipes/stopName';
+import {ToDatePipe} from '../../pipes/toDate';
+import {TimeAgoPipe} from 'angular2-moment';
+import * as moment from 'moment';
+// import * as moment from 'moment/moment';
 
+moment.lang('pt-br');
 @Component({
   selector: 'route-card',
-  templateUrl: 'build/components/route-card/route-card.html'
+  templateUrl: 'build/components/route-card/route-card.html',
+  pipes: [StopNamePipe, ToDatePipe, TimeAgoPipe]
 })
 
 export class RouteCard {
 
   @Input() routes: any[];
 
-  constructor(public nav: NavController) { }
+  constructor(public nav: NavController) {
+
+   }
 
   goToBusDetail(bus) {
     this.nav.push(RouteDetailsPage, bus);
