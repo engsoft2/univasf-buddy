adea72c8a2fe5a46e6aff77cc9d48fe6c2bb305c
Pedro Henrique <pedrosobralxv@gmail.com>
2016-05-26 12:03:26 -0300

    getting data from service just one time

--- app/pages/route-list/route-list.ts
+++ app/pages/route-list/route-list.ts
@@ -15,51 +15,38 @@ import {RouteService} from '../../providers/route-service/route-service';
 export class RouteListPage {
   searchQuery: string = '';
   routes;
+  data;
 
   constructor(public nav: NavController, public routeData : RouteService) {
-    // this.initializeItems();
+    this.initializeItems();
   }
 
-  ngOnInit() {
-    console.log('ngOnInit');
-    this.routeData.getAllLines().then(
-      data => {
-        this.routes = data;
-        console.log(data);
-      }
-    )
-  }
+  // ngOnInit() {
+  //   console.log('ngOnInit');
+  //   this.routeData.getAllLines().then(
+  //     data => {
+  //       this.routes = data;
+  //       console.log(data);
+  //     }
+  //   )
+  // }
 
   route(name) {
     return name;
   }
 
   initializeItems() {
-    this.routes= [{
-      name : "A", via: "Via Batalhão PM/Av. Nações",
-      stops: [
-        { hour: "6:10", loc: "UNIVASF Campus Juazeiro/BA" },
-        { hour: "6:12", loc: "Sanfra Motos" },
-        { hour: "6:15", loc: "UNIVASF Campus Juazeiro/BA" },
-        { hour: "6:20", loc: "Estação Velha / Cooperativa Brasil" },
-        { hour: "6:23", loc: "Farmácia Popular (João XXIII)" },
-        { hour: "6:25", loc: "Verdão" },
-        { hour: "6:27", loc: "Ao lado do Batalhão da PM" },
-        { hour: "6:30", loc: "Posto Raul Lins (Castelo Lins)" },
-        { hour: "6:32", loc: "Equipadora Cordeiro (Alto do Cruzeiro)" },
-        { hour: "6:35", loc: "Parque Lagoa de Calú" },
-        { hour: "6:40", loc: "Canteiro de Obras – Juazeiro" },
-        { hour: "7:05", loc: "Ciências Agrárias – CCA" },
-      ]
-    }, {
-      name: "B", via: "Via Av. Honorato Viana"
-    }];
+    this.routeData.getAllLines().then(
+      data => {
+        this.routes = data;
+        this.data = data;
+      }
+    );
   }
 
   search(searchbar) {
     // Reset items back to all of the items
-    // this.initializeItems();
-    this.ngOnInit();
+    this.routes = this.data;
 
     // set q to the value of the searchbar
     var q = searchbar.value;
@@ -75,6 +62,6 @@ export class RouteListPage {
         return true;
       }
       return false;
-    })
+    });
   }
 }
