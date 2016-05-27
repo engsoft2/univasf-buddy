c409f715335f15222c084e7a5d44a1b851d01d57
Pedro Henrique <pedrosobralxv@gmail.com>
2016-05-27 13:07:25 -0300

    formatando texto do card route

--- app/pipes/stopName.ts
+++ app/pipes/stopName.ts
@@ -9,14 +9,63 @@ export class StopNamePipe {
   /*
     Takes a value and makes it lowercase.
    */
-  transform(value: string, args: any[]) {
+  transform(value: string, args: string, hour: string) {
     value = value.toLowerCase();
-    if (value.indexOf('jua') > -1) {
-      return 'de Jua';
-    } else if (value.indexOf('petro') > -1) {
-      return 'de Petro';
+
+    if (args === 'inicial') {
+      console.log('inicial')
+      return this._FormatInicial(value, hour);
+    } else if (args === 'final') {
+      console.log('final')
+      return this._FormatFinal(value, hour);
+    }
+  }
+
+  _parseHour(date) {
+    let a = new Date();
+    let b = new Date();
+
+    b.setHours(Number(date.slice(0, 2)))
+    b.setMinutes(Number(date.slice(3, 5)))
+
+    return a > b;
+  }
+
+  _FormatInicial(name, hour) {
+    if (name.indexOf('jua') > -1) {
+      if (this._parseHour(hour)) {
+        return 'Saiu de Jua'
+      }
+      return 'Saindo de Jua';
+    } else if (name.indexOf('petro') > -1) {
+      if (this._parseHour(hour)) {
+        return 'Saiu de Petro'
+      }
+      return 'Saindo de Petro';
     } else {
-      return 'do CCA';
+      if (this._parseHour(hour)) {
+        return 'Saiu do CCA'
+      }
+      return 'Saindo do CCA';
+    }
+  }
+
+  _FormatFinal(name, hour) {
+    if (name.indexOf('jua') > -1) {
+      if (this._parseHour(hour)) {
+        return 'Chegou em Jua'
+      }
+      return 'Chegando em Jua';
+    } else if (name.indexOf('petro') > -1) {
+      if (this._parseHour(hour)) {
+        return 'Chegou em Petro'
+      }
+      return 'Chegando em Petro';
+    } else {
+      if (this._parseHour(hour)) {
+        return 'Chegou no CCA'
+      }
+      return 'Chegando no CCA';
     }
   }
 }
