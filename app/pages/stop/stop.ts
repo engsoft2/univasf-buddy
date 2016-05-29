e59e819214246a025bc0aec1f8640245d3d11260
Pedro Henrique <pedrosobralxv@gmail.com>
2016-05-28 22:18:03 -0300

    config search bar and its funcs

--- app/pages/stop/stop.ts
+++ app/pages/stop/stop.ts
@@ -5,5 +5,17 @@ import {Page, NavController} from 'ionic-angular';
 })
 
 export class StopPage {
+  private searchQuery: string;
+
   constructor(public nav: NavController) { }
+
+  private search(searchBar) {
+    // set q to the value of the searchbar
+    var q = searchBar.value;
+
+    // if the value is an empty string don't filter the items
+    if (q.trim() == '') {
+      return;
+    }
+  }
 }
