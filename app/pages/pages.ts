becd45d0a10800ca8785d2888910555bbfa6af7c
Pedro Henrique <pedrosobralxv@gmail.com>
2016-05-31 17:36:17 -0300

    export stop details page

--- app/pages/pages.ts
+++ app/pages/pages.ts
@@ -1,4 +1,5 @@
 export * from './route-details/route-details';
 export * from './route-list/route-list';
 export * from './stop/stop';
+export * from './stop-details/stop-details';
 export * from './tabs/tabs';
