62aa3b2bc50fc938e0da3c89a890ce7e3d6fa405
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-01 15:19:43 -0300

    Added: - Loading and Alert components; - Implemented goToRoutes function; - Save check if this.data is not null;

--- app/pages/stop/stop.ts
+++ app/pages/stop/stop.ts
@@ -1,6 +1,7 @@
-import {Page, NavController} from 'ionic-angular';
+import {Alert, Loading, Page, NavController} from 'ionic-angular';
 import {RouteService} from '../../providers/providers';
 import {StopModel} from '../../models/models';
+import {StopDetailsPage} from '../pages';
 
 @Page({
   templateUrl: 'build/pages/stop/stop.html',
@@ -14,14 +15,19 @@ export class StopPage {
   constructor(public nav: NavController, public routeData: RouteService) { }
 
   ngOnInit() {
+    let loading = Loading.create({
+      content: 'Please wait...'
+    });
+
+    this.presentLoadingDefault(loading);
     this.routeData.stops().then(data => {
-      // this.stops = data;
       this.data = data;
+      loading.dismiss();
     });
   }
 
   private goToRoutes(stop) {
-    // this.nav.push()
+    this.nav.push(StopDetailsPage, stop);
   }
 
   private search(searchBar) {
@@ -35,12 +41,40 @@ export class StopPage {
       return;
     }
 
-    this.stops = this.data.filter((v) => {
-      if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
-        return true;
-      }
-      return false;
-    });
+    // save check
+    if (this.data) {
+      this.stops = this.data.filter((v) => {
+        if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      });
+    }
   }
 
+  // TODO: think in a better approach to
+  // the loading component in all pages
+  presentLoadingDefault(loading) {
+    this.nav.present(loading);
+
+    setTimeout(() => {
+      if (!this.data) {
+        console.log('not ok');
+        this.presentAlert();
+      }
+
+      loading.dismiss();
+    }, 10000);
+  }
+
+  // TODO: present a Toast
+  // https://www.google.com/design/spec/patterns/errors.html#errors-app-errors
+  presentAlert() {
+    let alert = Alert.create({
+      title: 'Ocorreu uma falha ao carregar os dados',
+      subTitle: 'Por favor verifique sua conexao com a Internet',
+      buttons: ['Fechar']
+    });
+    this.nav.present(alert);
+  }
 }
