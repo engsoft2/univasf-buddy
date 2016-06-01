7d899a016e56dda81c2539e9a6e484bbe7f67181
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-01 15:24:14 -0300

    Added Loading, Alert components

--- app/pages/stop-details/stop-details.ts
+++ app/pages/stop-details/stop-details.ts
@@ -1,9 +1,56 @@
-import {Page, NavController} from 'ionic-angular';
+import {Alert, Loading, Page, NavController, NavParams} from 'ionic-angular';
+import {RouteService} from '../../providers/providers';
+import {StopModel} from '../../models/models';
+import {RouteCard}    from '../../components/components';
 
 @Page({
   templateUrl: 'build/pages/stop-details/stop-details.html',
+  directives: [RouteCard],
 })
 
 export class StopDetailsPage {
-  constructor(public nav: NavController) {}
+  stop: any;
+  routes: any;
+
+  constructor(public nav: NavController, params: NavParams, public service: RouteService) {
+    this.stop = params.data;
+  }
+
+  ngOnInit() {
+    let loading = Loading.create({
+      content: 'Please wait...'
+    });
+
+    this.presentLoadingDefault(loading);
+    this.service.getRoutes(this.stop.id).then(
+      data => {
+        this.routes = data;
+        loading.dismiss();
+      }
+    )
+  }
+
+  presentLoadingDefault(loading) {
+
+    this.nav.present(loading);
+
+    setTimeout(() => {
+      if (!this.routes) {
+        console.log('not ok')
+        this.presentAlert();
+      }
+
+      loading.dismiss();
+    }, 5000);
+  }
+
+  presentAlert() {
+    let alert = Alert.create({
+      title: 'Ocorreu uma falha ao carregar os dados',
+      subTitle: 'Por favor verifique sua conexao com a Internet',
+      buttons: ['Fechar']
+    });
+    this.nav.present(alert);
+  }
+
 }
