65ec6b9d7272e9a0b1fc6ed166182f29589f1719
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-06 23:01:04 -0300

    add service to get the news headlines and content

--- app/pages/pages.ts
+++ app/pages/pages.ts
@@ -3,3 +3,4 @@ export * from './route-list/route-list';
 export * from './stop/stop';
 export * from './stop-details/stop-details';
 export * from './tabs/tabs';
+export * from './news-list/news-list';
--- app/providers/route-service/route-service.ts
+++ app/providers/route-service/route-service.ts
@@ -22,6 +22,44 @@ export class RouteService {
     })
   }
 
+  get newsHeadlines() {
+    return this.loadNews().then(data => {
+      return data;
+    });
+  }
+
+  getNews(id) {
+    return this.loadNews(id).then(data => {
+      return data;
+    });
+  }
+
+  private loadNews(newsId?: number) {
+    // Base URI for Web service
+    let yql_base_uri = "https://query.yahooapis.com/v1/public/yql?q=";
+
+    let univasf_url = "'http://univasf.edu.br/todas.php'"
+    let univasf_xpath = "'//*[@id=\"formNoticia\"]/table[2]/tbody/tr/td/font/span'"
+
+    // if a news id is passed the request changes
+    if (newsId) {
+      univasf_url = "'http://univasf.edu.br/detalhe_noticias.php?cod=" + newsId + "'";
+      univasf_xpath = "'/html/body/table/tbody/tr/td/p'";
+    }
+
+    // Create a YQL query to get the data
+    let yql_query =   "select onclick, b, content from html where url="+
+                          univasf_url + " and xpath=" + univasf_xpath;
+
+    return new Promise(resolve => {
+      this.http.get(yql_base_uri+encodeURIComponent(yql_query)+'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+        .map(res => res.json())
+        .subscribe(data => {
+          resolve(data);
+        });
+    })
+  }
+
   getRoutesById(id) {
     let routes: Array<RouteModel> = undefined;
 
