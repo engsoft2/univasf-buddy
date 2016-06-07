efa0213798bd85ae2abdbcd7da6becd3e7d936be
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-06 23:00:35 -0300

    add news list page

---
+++ app/pages/news-list/news-list.ts
@@ -0,0 +1,14 @@
+import {Page, NavController} from 'ionic-angular';
+
+/*
+  Generated class for the NewsListPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/news-list/news-list.html',
+})
+export class NewsListPage {
+  constructor(public nav: NavController) {}
+}
