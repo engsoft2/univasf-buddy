50aed76e1482784240f886b18a2b0fdcc964c710
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-08 13:11:42 -0300

    update(tabs) update map to tabs page

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {StopPage, RouteListPage} from '../pages';
+import {StopPage, RouteListPage, MapPage} from '../pages';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -8,6 +8,7 @@ import {StopPage, RouteListPage} from '../pages';
 export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  tab1Root: any = StopPage;
-  tab2Root: any = RouteListPage;
+  tab1Root: any = MapPage;
+  tab2Root: any = StopPage;
+  tab3Root: any = RouteListPage;
 }
