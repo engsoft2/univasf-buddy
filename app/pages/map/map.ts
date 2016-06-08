f756525fa21c79b838d6c0440041850b3cbd3201
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-08 13:07:52 -0300

    feat(map) add map page

---
+++ app/pages/map/map.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/map/map.html',
+})
+
+export class MapPage {
+  constructor(public nav: NavController) {}
+}
--- app/pages/pages.ts
+++ app/pages/pages.ts
@@ -2,6 +2,7 @@ export * from './route-details/route-details';
 export * from './route-list/route-list';
 export * from './stop/stop';
 export * from './stop-details/stop-details';
+export * from './map/map';
 export * from './tabs/tabs';
 export * from './news-list/news-list';
 export * from './news-details/news-details';
