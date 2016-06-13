364967b5d2492a636160ae4ba9e95bbd1dd08710
Pedro Henrique <pedrosobralxv@gmail.com>
2016-06-13 00:36:32 -0300

    update(map): add init map methods

--- app/pages/map/map.ts
+++ app/pages/map/map.ts
@@ -6,5 +6,41 @@ import {NavController} from 'ionic-angular';
 })
 
 export class MapPage {
+  private map: any;
+
   constructor(public nav: NavController) {}
+
+  ngOnInit() {
+    this.initMap();
+  }
+
+  initMap() {
+    console.log('initMap');
+    let mapEle = document.getElementById('map-tab');
+
+    this.map = new google.maps.Map(mapEle, {
+      center: { lat: -9.42044, lng: -40.50367 },
+      zoomControl: true,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: true,
+      rotateControl: true,
+      fullscreenControl: true,
+      zoom: 14
+    });
+
+    let layer = new google.maps.FusionTablesLayer({
+      query: {
+        select: "col2",
+        from: "1LBauH-AOJ15r1uAEmnctJYhfQfpNJtdBcJFftuak",
+        where: ""
+      },
+      options: {
+        styleId: 2,
+        templateId: 2
+      }
+    });
+
+    layer.setMap(this.map);
+  }
 }
